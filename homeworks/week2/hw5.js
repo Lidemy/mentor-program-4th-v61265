3c064d2abb1831c846e1c32d33b2141d67d4ab24
v61265 <v61265@gmail.com>
2020-06-30 01:29:36 +0800

    week2作業

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,22 @@
 function join(arr, concatStr) {
-  
+  var result = arr[0]
+  for(i = 1; i < arr.length; i++){
+    result += concatStr + arr[i]
+  }
+  return result
 }
 
 function repeat(str, times) {
-  
+  var result = ''
+  for(j = 1; j <= times; j++){
+    result += str
+  }
+  return result
 }
 
 console.log(join(['a'], '!'));
+console.log(join([1, 2, 3], ''));
+console.log(join(['a', 'b', 'c'], '!'));
+console.log(join(['a', 1, 'b', 2, 'c', 3], ','));
 console.log(repeat('a', 5));
+console.log(repeat('yoyo', 2))
